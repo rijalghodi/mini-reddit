b6c98d36a754d3f0d2f262b563ff4a839c9f3b72
Rijal Ghodi <rijalghodi10@gmail.com>
2022-10-24 07:55:17 +0700

    edit-icons-succeed

--- src/components/Post/Post.js
+++ src/components/Post/Post.js
@@ -1,16 +1,23 @@
 import React, { useState } from "react";
 import "./Post.css";
-import { BiComment } from "react-icons/bi";
+import { BiComment, BiCommentDetail } from "react-icons/bi";
 import { VscAccount } from "react-icons/vsc";
-import { BsBookmark } from "react-icons/bs";
+import { BsBookmark, BsBookmarkCheck } from "react-icons/bs";
+import { FaCommentAlt, FaRegCommentAlt } from "react-icons/fa";
+
 import Vote from "../Vote/Vote";
 import Comments from "../../features/Comments/Comments";
 
 export default function Post({ post }) {
   const [showComments, setShowComments] = useState(false);
+  const [bookmarked, setBookmarked] = useState(false);
 
-  const handleCommentButtonClick = () => {
+  const handleClickCommentButton = () => {
     showComments ? setShowComments(false) : setShowComments(true);
+  };
+
+  const handleClickBookmarkButton = () => {
+    bookmarked ? setBookmarked(false) : setBookmarked(true);
   };
 
   return (
@@ -26,13 +33,13 @@ export default function Post({ post }) {
         </div>
       )}
       <div className="bottom-bar">
-        <button className="comment-section flex-standard" onClick={handleCommentButtonClick}>
-          <BiComment style={{ fontSize: "1.8rem" }} />
+        <button className="comment-section flex-standard" onClick={handleClickCommentButton}>
+          {showComments ? <BiCommentDetail style={{ fontSize: "1.8rem" }} /> : <BiComment style={{ fontSize: "1.8rem" }} />}
           32k
         </button>
         <Vote voteUps={post.ups} />
-        <button className="bookmark">
-          <BsBookmark style={{ fontSize: "1.7rem" }} />
+        <button className="bookmark" onClick={handleClickBookmarkButton}>
+          {bookmarked ? <BsBookmarkCheck style={{ fontSize: "1.7rem" }} /> : <BsBookmark style={{ fontSize: "1.7rem" }} />}
         </button>
       </div>
       {showComments && <Comments permalink={post.permalink} postId={post.name} />}
